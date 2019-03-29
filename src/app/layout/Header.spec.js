467ad1cc31e2e392cda82d417ad9379973fa2f79
Ben Scott <ben@goodbomb.com>
2019-03-29 09:44:41 -0700

    Update Header.spec tests so they pass

--- src/app/layout/Header.spec.js
+++ src/app/layout/Header.spec.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
+import Header from './Header';
 
-import {
-    default as HeaderComponent,
-    Header } from './HeaderComponent';
-
-describe('HeaderComponent', function() {
-    it('should render without throwing an error', function() {
-        expect(shallow(<HeaderComponent />).contains(<Header className="header">Header content</Header>)).toBe(true);
-    });
-
+describe('Header', function() {
     it('should be selectable by class "header"', function() {
-        expect(shallow(<HeaderComponent />).is('.header')).toBe(true);
+        expect(shallow(<Header />).is('.header')).toBe(true);
     });
 
     it('should mount in a full DOM', function() {
-        expect(mount(<HeaderComponent />).length).toBe(1);
+        expect(mount(<Header />).length).toBe(1);
     });
 
     it('should render to static HTML', function() {
-        expect(render(<HeaderComponent />).text()).toEqual('Header content');
+        expect(render(<Header />).text()).toEqual('Header content');
     });
 });
